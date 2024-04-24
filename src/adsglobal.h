9f191e9d30b8587c5bb8d6c742912e68a32e5906
bmseven <yuanxianchaoxc@163.com>
2024-04-24 17:40:08 +0800

    初始化项目框架

---
+++ src/adsglobal.h
@@ -0,0 +1,32 @@
+#ifndef ADSGLOBAL_H
+#define ADSGLOBAL_H
+
+#include <QObject>
+#include <QFlags>
+
+#if defined(ADS_IMPORT)
+    #define ADS_EXPORT_API
+#elif defined(ADS_EXPORT)
+    #define ADS_EXPORT_API Q_DECL_EXPORT
+#else
+    #define ADS_EXPORT_API Q_DECL_IMPORT
+#endif
+
+#ifdef ADS_DEBUG_PRINT
+    #define ADS_PRINT(s) qDebug() << s
+#else
+    #define ADS_PRINT(s)
+#endif
+
+// ads 命名空间
+#ifdef ADS_NAMESPACE_ENABLED
+    #define ADS_BEGIN_NAMESPACE namespace ads {
+    #define ADS_END_NAMESPACE }
+    #define ADS_NS ::ads
+#else
+    #define ADS_BEGIN_NAMESPACE
+    #define ADS_END_NAMESPACE
+    #define ADS_NS
+#endif
+
+#endif // ADSGLOBAL_H
